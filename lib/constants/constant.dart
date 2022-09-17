813131f570eb8e6a744db8bb6a7dbd645a184773
Sigit Setiaji <setiaji24@gmail.com>
2022-09-18 06:38:18 +0700

    update layout ui & function bmi calculator

---
+++ lib/constants/constant.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
+Color primaryColor = const Color(0xff0a0e21);
+
+TextStyle labelTextStyle = const TextStyle(
+  fontSize: 18,
+  color: Colors.white, //Color(0xff8d8398),
+);
+
+TextStyle numberTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
+);
+
+const String underweightSevere = "Underweight (Severe thinness)";
+const String underweightModerate = "Underweight (Moderate thinness)";
+const String underweightMild = "Underweight (Mild thinness)";
+const String normal = "Normal";
+const String overweight = "Overweight";
+const String obeseI = "Obese (Class I)";
+const String obeseII = "Obese (Class II)";
+const String obeseIII = "Obese (Class III)";
---
+++ lib/views/bmi_result_screen.dart
@@ -0,0 +1,154 @@
+import 'package:bmi_calculator_edspert/constants/constant.dart';
+import 'package:bmi_calculator_edspert/views/bmi_data_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BmiResultScreen extends StatelessWidget {
+  const BmiResultScreen({super.key, required this.bmi});
+
+  final double bmi;
+
+  String determineBmiCategory(double bmiValue) {
+    String category = "";
+    if (bmiValue < 16.0) {
+      category = underweightSevere;
+    } else if (bmiValue < 17) {
+      category = underweightModerate;
+    } else if (bmiValue < 18.5) {
+      category = underweightMild;
+    } else if (bmiValue < 25) {
+      category = normal;
+    } else if (bmiValue < 30) {
+      category = overweight;
+    } else if (bmiValue < 35) {
+      category = obeseI;
+    } else if (bmiValue < 40) {
+      category = obeseII;
+    } else if (bmiValue >= 40) {
+      category = obeseIII;
+    }
+    return category;
+  }
+
+  String getHealRiskDescription(String categoryName) {
+    String desc = "";
+    switch (categoryName) {
+      case underweightSevere:
+        desc = "Possible nutritional deficiency and osteoporosis.";
+        break;
+      case underweightModerate:
+        desc = "Possible nutritional deficiency and osteoporosis.";
+        break;
+      case underweightMild:
+        desc = "Possible nutritional deficiency and osteoporosis.";
+        break;
+      case normal:
+        desc = "Low risk (healthy range).";
+        break;
+      case overweight:
+        desc =
+            "Moderate risk of developing heart disease, high blood pressure, stroke, diabetes mellitus.";
+        break;
+      case obeseI:
+        desc =
+            "Hight risk of developing heart disease, high blood pressure, stroke, diabetes mellitus.";
+        break;
+      case obeseII:
+        desc =
+            "Hight risk of developing heart disease, high blood pressure, stroke, diabetes mellitus.";
+        break;
+      case obeseIII:
+        desc =
+            "Hight risk of developing heart disease, high blood pressure, stroke, diabetes mellitus.";
+        break;
+      default:
+    }
+    return desc;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bmiCategory = determineBmiCategory(bmi);
+    final bmiDesc = getHealRiskDescription(bmiCategory);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hasil Hitung BMI"),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              child: Center(
+                child: Text(
+                  "Hasil Perhitungan",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: BmiCard(
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      bmiCategory,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      "${bmi.toStringAsFixed(1)}",
+                      style: TextStyle(
+                        fontSize: 100,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      bmiDesc,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Container(
+              height: 80,
+              color: Color(0xffec3c66),
+              child: Center(
+                child: Text(
+                  "Hitung Ulang",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
